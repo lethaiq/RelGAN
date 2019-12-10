b1e9542e86840e248e870d885e04fbf27b9e1387
thai le <thai.le@ist.psu.edu>
2019-12-10 10:59:12 -0500

    updates

--- models/rmc_att.py
+++ models/rmc_att.py
@@ -6,6 +6,28 @@ from utils.ops import *
 
 def generator(x_real, temperature, vocab_size, batch_size, seq_len, gen_emb_dim, mem_slots, head_size, num_heads,
               hidden_dim, start_token):
+
+    def _pretrain_recurrence(i, x_t, h_tm1, g_predictions):
+        mem_o_t, h_t = gen_mem(x_t, h_tm1)
+        o_t = g_output_unit(mem_o_t)
+        g_predictions = g_predictions.write(i, tf.nn.softmax(o_t))  # batch_size x vocab_size
+        x_tp1 = ta_emb_x.read(i)
+        return i + 1, x_tp1, h_t, g_predictions
+        
+    def _gen_recurrence(i, x_t, h_tm1, gen_o, gen_x, gen_x_onehot_adv):
+        mem_o_t, h_t = gen_mem(x_t, h_tm1)  # hidden_memory_tuple
+        o_t = g_output_unit(mem_o_t)  # batch x vocab, logits not prob
+        gumbel_t = add_gumbel(o_t)
+        next_token = tf.cast(tf.argmax(gumbel_t, axis=1), tf.int32)
+        x_onehot_appr = tf.nn.softmax(tf.multiply(gumbel_t, temperature))  # one-hot-like, [batch_size x vocab_size]
+        # x_tp1 = tf.matmul(x_onehot_appr, g_embeddings)  # approximated embeddings, [batch_size x emb_dim]
+        x_tp1 = tf.nn.embedding_lookup(g_embeddings, next_token)  # embeddings, [batch_size x emb_dim]
+        gen_o = gen_o.write(i, tf.reduce_sum(tf.multiply(tf.one_hot(next_token, vocab_size, 1.0, 0.0),
+                                                         tf.nn.softmax(o_t)), 1))  # [batch_size] , prob
+        gen_x = gen_x.write(i, next_token)  # indices, [batch_size]
+        gen_x_onehot_adv = gen_x_onehot_adv.write(i, x_onehot_appr)
+        return i + 1, x_tp1, h_t, gen_o, gen_x, gen_x_onehot_adv
+
     start_tokens = tf.constant([start_token] * batch_size, dtype=tf.int32)
     output_size = mem_slots * head_size * num_heads
 
@@ -22,20 +44,6 @@ def generator(x_real, temperature, vocab_size, batch_size, seq_len, gen_emb_dim,
     gen_x = tensor_array_ops.TensorArray(dtype=tf.int32, size=seq_len, dynamic_size=False, infer_shape=True)
     gen_x_onehot_adv = tensor_array_ops.TensorArray(dtype=tf.float32, size=seq_len, dynamic_size=False,
                                                     infer_shape=True)
-
-    def _gen_recurrence(i, x_t, h_tm1, gen_o, gen_x, gen_x_onehot_adv):
-        mem_o_t, h_t = gen_mem(x_t, h_tm1)  # hidden_memory_tuple
-        o_t = g_output_unit(mem_o_t)  # batch x vocab, logits not prob
-        gumbel_t = add_gumbel(o_t)
-        next_token = tf.cast(tf.argmax(gumbel_t, axis=1), tf.int32)
-        x_onehot_appr = tf.nn.softmax(tf.multiply(gumbel_t, temperature))  # one-hot-like, [batch_size x vocab_size]
-        # x_tp1 = tf.matmul(x_onehot_appr, g_embeddings)  # approximated embeddings, [batch_size x emb_dim]
-        x_tp1 = tf.nn.embedding_lookup(g_embeddings, next_token)  # embeddings, [batch_size x emb_dim]
-        gen_o = gen_o.write(i, tf.reduce_sum(tf.multiply(tf.one_hot(next_token, vocab_size, 1.0, 0.0),
-                                                         tf.nn.softmax(o_t)), 1))  # [batch_size] , prob
-        gen_x = gen_x.write(i, next_token)  # indices, [batch_size]
-        gen_x_onehot_adv = gen_x_onehot_adv.write(i, x_onehot_appr)
-        return i + 1, x_tp1, h_t, gen_o, gen_x, gen_x_onehot_adv
 
     _, _, _, gen_o, gen_x, gen_x_onehot_adv = control_flow_ops.while_loop(
         cond=lambda i, _1, _2, _3, _4, _5: i < seq_len,
@@ -56,13 +64,6 @@ def generator(x_real, temperature, vocab_size, batch_size, seq_len, gen_emb_dim,
     ta_emb_x = tensor_array_ops.TensorArray(dtype=tf.float32, size=seq_len)
     ta_emb_x = ta_emb_x.unstack(x_emb)
 
-    def _pretrain_recurrence(i, x_t, h_tm1, g_predictions):
-        mem_o_t, h_t = gen_mem(x_t, h_tm1)
-        o_t = g_output_unit(mem_o_t)
-        g_predictions = g_predictions.write(i, tf.nn.softmax(o_t))  # batch_size x vocab_size
-        x_tp1 = ta_emb_x.read(i)
-        return i + 1, x_tp1, h_t, g_predictions
-
     _, _, _, g_predictions = control_flow_ops.while_loop(
         cond=lambda i, _1, _2, _3: i < seq_len,
         body=_pretrain_recurrence,
@@ -79,7 +80,7 @@ def generator(x_real, temperature, vocab_size, batch_size, seq_len, gen_emb_dim,
         )
     ) / (seq_len * batch_size)
 
-    return gen_x_onehot_adv, gen_x, pretrain_loss
+    return gen_x_onehot_adv, gen_x, pretrain_loss, gen_o
 
 
 def discriminator(x_onehot, batch_size, seq_len, vocab_size, dis_emb_dim, num_rep, sn):
